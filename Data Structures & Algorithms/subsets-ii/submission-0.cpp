7d52abb5c33ea3bbdba08c777cc03f648a391cd1
shishir jaiswal <shishir210205@gmail.com>
2026-06-09 15:52:15 +0530

    Add: subsets-ii - submission-0

---
+++ Data Structures & Algorithms/subsets-ii/submission-0.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+
+    void backtrack(int index, vector<int>& nums, vector<int>& curr) {
+        ans.push_back(curr);
+
+        for (int i = index; i < nums.size(); i++) {
+            // Skip duplicates at the same level
+            if (i > index && nums[i] == nums[i - 1])
+                continue;
+
+            curr.push_back(nums[i]);
+            backtrack(i + 1, nums, curr);
+            curr.pop_back();
+        }
+    }
+
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+
+        vector<int> curr;
+        backtrack(0, nums, curr);
+
+        return ans;
+    }
+};
